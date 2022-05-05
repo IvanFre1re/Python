5470b8240696275b1fca8cad7df0e5ae9c9c5290
IvanFre1re <ivanmedeiros0903@outlook.com>
2022-05-04 23:24:41 -0300

    Dia 1- Começo da Jornada

---
+++ UDEMY/day1.py
@@ -0,0 +1,60 @@
+# print("Hello\n word")
+
+# i started with a simple hello word just to rememeber the syntax 
+
+# print ("Hello" + " " + "Word")
+
+# Another print but with input
+
+# print ("Hello" + " " + input("What's your name?\n"))
+
+# Exercise {
+
+# x = input("Type your name\n")
+
+# print(len(x))
+
+# Teacher made that way -- print ( len( input("Whats your name? \n")))
+
+#} ok!
+
+# exercise 2 {{}
+
+#a = input("a value\n")
+#b = input("b value\n")
+
+#c = a
+
+#a = b
+
+#b = c
+
+
+
+#print("A value is" + " " + a + " and B value is equal to" + " " + c)
+
+# I didnt understand this one 
+
+
+# Exercise final day 1
+
+print("Hello user")
+
+name_user = input("Tell me the city you grew up\n")
+
+print("Ok, i got it" + ' ' + 'Just one more question\n')
+
+pet_name = input("Tell  me the name of a pet\n")
+
+print("Well, so you grew up on" + ' ' + name_user + ' and you choose' + ' ' + pet_name + ' for yours pet name')
+
+
+# Exercise ok!
+
+
+
+
+
+
+
+
---
+++ UDEMY/help.md
@@ -0,0 +1,6 @@
+\n serve pra cortar a linha como um <br>
+
+Também é possível colocar varias "Tags" dentro de outras tipo 
+
+print (len (input(" Coisas ") ) )
+
